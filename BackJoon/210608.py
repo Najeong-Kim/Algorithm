6ba57598fd9ac682347c06492d4a900478c3c0d2
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-06-08 23:22:02 +0900

    210608

---
+++ BackJoon/210608.py
@@ -0,0 +1,16 @@
+# 1000 A+B
+
+# a, b = map(int, input().split())
+# print(a + b)
+
+
+# 1001 A-B
+
+a, b = map(int, input().split())
+print(a - b)
+
+
+# 1008 A/B
+
+a, b = map(int, input().split())
+print(a / b)
