969f68514e36e37dcf5b0a1ed636dbbe994528d1
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-06-18 01:08:39 +0900

    210617

--- BackJoon/210617.py
+++ BackJoon/210617.py
@@ -34,3 +34,138 @@
 #
 # for i in li_sorted:
 #     print(i)
+
+
+# 15650 N과 M (2)
+
+# import sys
+# from itertools import combinations
+#
+# input = sys.stdin.readline
+#
+# N, M = map(int, input().rstrip().split())
+# arr = []
+#
+# for i in range(1, N + 1):
+#     arr.append(i)
+#
+# arr_com = list(combinations(arr, M))
+#
+# for i in arr_com:
+#     for j in i:
+#         print(j, end=' ')
+#     print('')
+
+
+# 15652 N과 M (4)
+
+# import sys
+# from itertools import combinations_with_replacement
+#
+# input = sys.stdin.readline
+#
+# N, M = map(int, input().rstrip().split())
+# arr = []
+#
+# for i in range(1, N + 1):
+#     arr.append(i)
+#
+# arr_com = list(combinations_with_replacement(arr, M))
+#
+# for i in arr_com:
+#     for j in i:
+#         print(j, end=' ')
+#     print('')
+
+
+# 15654 N과 M (5)
+
+# import sys
+# from itertools import permutations
+#
+# input = sys.stdin.readline
+#
+# N, M = map(int, input().rstrip().split())
+# arr = list(map(int, input().rstrip().split()))
+#
+# arr.sort()
+#
+# arr_com = list(permutations(arr, M))
+#
+# for i in arr_com:
+#     for j in i:
+#         print(j, end=' ')
+#     print('')
+
+
+# 15657 N과 M (8)
+
+# import sys
+# from itertools import combinations_with_replacement
+#
+# input = sys.stdin.readline
+#
+# N, M = map(int, input().rstrip().split())
+# arr = list(map(int, input().rstrip().split()))
+#
+# arr.sort()
+#
+# arr_com = list(combinations_with_replacement(arr, M))
+#
+# for i in arr_com:
+#     for j in i:
+#         print(j, end=' ')
+#     print('')
+
+
+# 15663 N과 M (9)
+
+# import sys
+# from itertools import permutations
+#
+# input = sys.stdin.readline
+#
+# N, M = map(int, input().rstrip().split())
+# arr = list(map(int, input().rstrip().split()))
+#
+# arr_com = list(permutations(arr, M))
+# result = set()
+#
+# for i in arr_com:
+#     result.add(i)
+#
+# for i in range(1, len(arr_com[0]) + 1):
+#     result = sorted(result, key= lambda x: x[-i])
+#
+# for i in result:
+#     for j in i:
+#         print(j, end=' ')
+#     print('')
+
+
+# 15666 N과 M (12)
+
+# import sys
+# from itertools import combinations_with_replacement
+#
+# input = sys.stdin.readline
+#
+# N, M = map(int, input().rstrip().split())
+# arr = list(map(int, input().rstrip().split()))
+#
+# arr.sort()
+#
+# arr_com = list(combinations_with_replacement(arr, M))
+# result = set()
+#
+# for i in arr_com:
+#     result.add(i)
+#
+# for i in range(1, len(arr_com[0]) + 1):
+#     result = sorted(result, key= lambda x: x[-i])
+#
+# for i in result:
+#     for j in i:
+#         print(j, end=' ')
+#     print('')
+
