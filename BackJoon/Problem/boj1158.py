3ba81ee523e54debb26733270dea10e820049f32
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-12-05 20:33:48 +0900

    solve: boj1158

---
+++ BackJoon/Problem/boj1158.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N, K = map(int, input().split())
+queue = deque()
+array = []
+
+for i in range(1, N+1):
+    queue.append(str(i))
+
+while queue:
+    for i in range(K-1):
+        now = queue.popleft()
+        queue.append(now)
+    array.append(queue.popleft())
+
+print('<' + ', '.join(array) + '>')
