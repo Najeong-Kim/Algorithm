1e1835599c5edf6e5376f7e6b61ce2cb6e19454f
Najeong-Kim <atpeoe1023@gmail.com>
2026-06-03 23:42:53 +0900

    solve: 11659

---
+++ loopers-coding-test-challenge-s3/04-prefix-sum/11659.py
@@ -0,0 +1,7 @@
+N, M = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+for i in range(1, N + 1):
+    arr[i] += arr[i - 1]
+for _ in range(M):
+    i, j = map(int, input().split())
+    print(arr[j] - arr[i - 1])
